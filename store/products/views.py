04b5c7b8df10347cf0940b4de43c86f28a0773f8
gaiijinn <vladruban8@gmail.com>
2023-11-12 16:42:09 +0300

    update_media&load_from_db

--- store/products/views.py
+++ store/products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from products.models import ProductCategory, Product
 
 # Create your views here.
 
@@ -14,42 +15,8 @@ def index(request):
 def products(request):
     context = {
         "title": "Каталог",
-        "products": [
-            {'img': "/static/vendor/img/products/Blue-jacket-The-North-Face.png",
-             "name": "Синяя куртка The North Face.",
-             "price": "23 725,00 руб.",
-             "description": "Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель."},
-
-            {'img': "/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png",
-             "name": "Коричневый спортивный oversized-топ ASOS DESIGN.",
-             "price": "3 390,00 руб",
-             "description": "Материал с плюшевой текстурой. Удобный и мягкий."},
-
-            {'img': "/static/vendor/img/products/Black-Nike-Heritage-backpack.png",
-             "name": "Черный рюкзак Nike Heritage",
-             "price": "2 340,00 руб.",
-             "description": "Плотная ткань. Легкий материал."},
-
-            {'img': "/static/vendor/img/products/Black-Dr-Martens-shoes.png",
-             "name": "Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex",
-             "price": "13 590,00 руб",
-             "description": "Гладкий кожаный верх. Натуральный материал."},
-
-            {'img': "/static/vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png",
-             "name": "Темно-синие широкие строгие брюки ASOS DESIGN",
-             "price": "2 890,00 руб.",
-             "description": "Легкая эластичная ткань сирсакер Фактурная ткань.."},
-
-            {'img': "/static/vendor/img/products/Adidas-hoodie.png",
-             "name": "Худи черного цвета с монограммами adidas Originals",
-             "price": "6 090,00 руб",
-             "description": "Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни."},
-
-        ],
-        "some_info": [
-            "Новинки", "Одежда", "Обувь", "Акссесуары", "Подарки"
-        ],
-
+        "products": Product.objects.all(),
+        'category': ProductCategory.objects.all(),
     }
     return render(request, 'products/products.html', context=context)
 
--- store/store/urls.py
+++ store/store/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 from products.views import index, products, some_test
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,5 @@ urlpatterns = [
     re_path(r'^test', some_test)
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
