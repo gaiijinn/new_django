e97ef9765aee0edec241cbaacb4144e6468165ef
gaiijinn <vladruban8@gmail.com>
2023-11-26 14:22:57 +0200

    form_reg/log

--- store/users/views.py
+++ store/users/views.py
@@ -2,21 +2,21 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth
 from django.urls import reverse
 # Create your views here.
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
-    if request.method == "POST":
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
+    if request.method == "POST": #проверка являеться ли запрос типа ПОСТ
+        form = UserLoginForm(data=request.POST) #получаем данные которые были заполнены
+        if form.is_valid(): #валидация
             username = request.POST['username']
             password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-
+            user = auth.authenticate(username=username, password=password) #аутентификация проверяем есть ли юзер в бд
             if user:
-                auth.login(request, user)
-                return HttpResponseRedirect(reverse('index'))
-    else:
+                auth.login(request, user) #авторизация
+                return HttpResponseRedirect(reverse('index')) #переадресация
+
+    else: #если гет
         form = UserLoginForm()
 
     context = {
@@ -27,4 +27,17 @@ def login(request):
 
 
 def registration(request):
-    return render(request, 'users/registration.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save() #сохранит обьект в бд
+            return HttpResponseRedirect(reverse('users:login'))
+        else:
+            return HttpResponseRedirect(reverse('index'))  # переадресация
+    else:
+        form = UserRegistrationForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'users/registration.html', context)
