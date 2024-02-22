b01936739fb9a2299711d50bde4d46dcaa471be6
gaiijinn <vladruban8@gmail.com>
2024-02-22 23:52:06 +0200

    new_app&updated_db

--- store/store/settings.py
+++ store/store/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'products',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#users
+
+AUTH_USER_MODEL = 'users.User'
