af7364d6a4777bd695eff98c0c81df6a06a476d3
gaiijinn <vladruban8@gmail.com>
2024-02-24 00:08:11 +0200

    created_login_form

--- store/users/views.py
+++ store/users/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+
+
+from users.models import User
+from users.forms import UserLoginForm
 
 
 # Create your views here.
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+
+        if form.is_valid(): #если правильно ввел
+            username = request.POST['username'] #достаем данные которые получили
+            password = request.POST['password']
+
+            user = auth.authenticate(username=username, password=password) #проверяем юзера
+
+            if user: #если такой юзер есть логинимся
+                print("login succes")
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+
+    else:                         #когда чел заходит на логин
+        form = UserLoginForm()
+
+    context = {'form': form}
+
+    return render(request, 'users/login.html', context=context)
 
 
 def registration(request):
