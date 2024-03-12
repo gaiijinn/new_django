86bffa7bbc82aeb4979ff35d12391c926e9e9aa8
gaiijinn <vladruban8@gmail.com>
2024-03-13 00:27:02 +0200

    postgre_sql + updated fixtures folder

--- store/store/settings.py
+++ store/store/settings.py
@@ -82,11 +82,14 @@ WSGI_APPLICATION = 'store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'my_store_db',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
