9c7d119757a2bda1ed64bee14b8764aab94a0e22
gaiijinn <vladruban8@gmail.com>
2024-03-15 22:18:06 +0200

    test for test_list, test_list_categor

--- store/products/tests.py
+++ store/products/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
 from http import HTTPStatus
+from products.models import Product, ProductCategory
+
 
 import os
 import django
@@ -18,3 +20,34 @@ class IndexViewTestCase(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.context_data['title'], 'Магазин')
         self.assertTemplateUsed(response, 'products/index.html')
+
+
+class ProductsListViewTestCASE(TestCase):
+    fixtures = ['cats.json', 'prod.json']
+
+    def setUp(self):
+        self.product = Product.objects.all()
+
+    def test_list(self):
+        path = reverse('products:index')
+        response = self.client.get(path=path)
+
+        self._common_test(response=response)
+        self.assertEqual(list(response.context_data['object_list']), list(self.product[:3]))
+
+    def test_list_with_category(self):
+        category = ProductCategory.objects.first()
+        path = reverse('products:category', kwargs={'category_id': category.id})
+        response = self.client.get(path=path)
+
+        self._common_test(response=response)
+        self.assertEqual(
+            list(response.context_data['object_list']),
+            list(self.product.filter(category_id=category.id))
+        )
+
+    def _common_test(self, response):
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Каталог')
+        self.assertTemplateUsed(response, 'products/products.html')
+
