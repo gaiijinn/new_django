4f46f91ada7932d3a8e6e9c53824376abe034c16
gaiijinn <vladruban8@gmail.com>
2024-04-25 19:43:30 +0300

    order CBV + forms + model + admin

---
+++ store/orders/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from orders.models import Order
+
+
+class OrderForm(forms.ModelForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Введите имя получателя',
+        'type': 'text',
+    }), required=True)
+
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'id': 'lastName',
+        'placeholder': 'Введите фамилию получателя',
+        'type': 'text'
+    }), required=True)
+
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'id': 'email',
+        'placeholder': 'you@example.com',
+        'type': 'email'
+    }), required=True)
+
+    address = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'id': 'address',
+        'placeholder': 'Украина, Житомир, шевченко 107',
+        'type': 'text'
+    }), required=True)
+
+    class Meta:
+        model = Order
+        fields = ('first_name', 'last_name', 'email', 'address')
--- store/orders/urls.py
+++ store/orders/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from orders.views import orderCreateView
+from orders.views import OrderCreateView, orders_show
 
 app_name = 'orders'
 
 urlpatterns = [
-    path('order-create', orderCreateView.as_view(), name='order-create'),
+    path('order-create', OrderCreateView.as_view(), name='order_create'),
+    path('orders_history/', orders_show.as_view(), name='orders_history'),
 ]
