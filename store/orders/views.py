9adc85efa86758fc3699a92d3fddc376a9cf8ecd
gaiijinn <vladruban8@gmail.com>
2024-04-28 00:18:51 +0300

    stripe webhook

--- store/orders/views.py
+++ store/orders/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.views.generic.edit import CreateView
 from django.views.generic.base import TemplateView
+from django.views.decorators.csrf import csrf_exempt
 from orders.forms import OrderForm
 from django.urls import reverse_lazy, reverse
 from common.views import TitleMixin
@@ -32,16 +33,55 @@ class OrderCreateView(TitleMixin, CreateView):
             line_items=[
                 {
                     # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                    'price': 'price_1PAH1y06W1tJrlnm8Sga8ApO',
+                    'price': 'price_1PAHWe06W1tJrlnmdKUZJzo8',
                     'quantity': 1,
                 },
             ],
+            metadata={'order_id': self.object.id},
             mode='payment',
             success_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:order_success')),
             cancel_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:order_cancel')),
         )
 
         return HttpResponseRedirect(checkout_session.url, status=HTTPStatus.SEE_OTHER)
+
+
+@csrf_exempt
+def stripe_webhook_view(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        # Retrieve the session. If you require line items in the response, you may include them by expanding line_items.
+        session = stripe.checkout.Session.retrieve(
+            event['data']['object']['id'],
+            expand=['line_items'],
+        )
+
+        line_items = session.line_items
+        # Fulfill the purchase...
+        fulfill_order(line_items)
+
+    # Passed signature verification
+    return HttpResponse(status=200)
+
+
+def fulfill_order(line_items):
+  # TODO: fill me in
+  print("Fulfilling order")
 
 
 class SuccessTemplateView(TitleMixin, TemplateView):
